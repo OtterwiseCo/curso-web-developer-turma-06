8e83fdc86ba2f90608c6f7ce28e754dd5ddf1bb0
gsbarross <gabrieldasilvabarros1995@gmail.com>
2022-04-29 19:51:00 -0300

    Estudo de Caso Final

--- Semana 11-2/estudo-de-caso/src/routes/ProtectedPage.js
+++ Semana 11-2/estudo-de-caso/src/routes/ProtectedPage.js
@@ -1,6 +1,20 @@
 import React from "react";
+import { useEffect } from "react";
+import { listPets } from "../services/pets";
 
 function ProtectedPage() {
+  useEffect(() => {
+    const request = async () => {
+      try {
+        const response = await listPets();
+        console.log(response.data);
+      } catch (error) {
+        console.log("Deu Ruim");
+      }
+    };
+    request();
+  }, []);
+
   return <div>ProtectedPage</div>;
 }
 

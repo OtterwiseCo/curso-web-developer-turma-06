90e56a3c7ff76b8d990a05215526624271aa33d7
gsbarross <gabrieldasilvabarros1995@gmail.com>
2022-05-10 07:36:37 -0300

    Aula Teórico-Prática  Semana 13

---
+++ Semana 13/exemplo-prisma/src/index.js
@@ -0,0 +1,19 @@
+// ESM
+import Fastify from "fastify";
+const fastify = Fastify({
+  logger: true,
+});
+
+fastify.get("/", async (request, reply) => {
+  reply.send({ posts: [] });
+});
+
+const start = async () => {
+  try {
+    await fastify.listen(3000);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+start();

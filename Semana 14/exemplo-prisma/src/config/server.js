61ffd8ee383868a43c3153d6212a25ffb0cff2b1
gsbarross <gabrieldasilvabarros1995@gmail.com>
2022-05-16 19:42:31 -0300

    Aula Teórico-Prática Semana 14

---
+++ Semana 14/exemplo-prisma/src/config/server.js
@@ -0,0 +1,11 @@
+// ESM
+import Fastify from "fastify";
+import postsRoutes from "../routes/posts-routes.js";
+
+const fastify = Fastify({
+  logger: true,
+});
+
+fastify.register(postsRoutes);
+
+export default fastify;
